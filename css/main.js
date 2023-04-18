2a87d5986f4f9bceea994e08619cb1c203f40d48
Kalila L <somnilibertas@gmail.com>
2023-04-18 23:58:10 +0800

    Update main.js

--- css/main.js
+++ css/main.js
@@ -16,3 +16,61 @@ main_parent.addEventListener('click', function(e) {
         extensions.style.display = 'none';
     }
 });
+
+// TESTING CODE
+
+async function watchAndGetFirstBotMessage() {
+    let lastMessageCount = 0;
+  
+    function stripHTMLTags(html) {
+      const temporaryElement = document.createElement('div');
+      temporaryElement.innerHTML = html;
+      return temporaryElement.textContent || temporaryElement.innerText || '';
+    }
+  
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+  
+    async function getFirstMessageBody() {
+      const messageDivs = document.querySelectorAll('.chat .message');
+      const botMessageDivs = Array.from(messageDivs).filter(messageDiv => {
+        return messageDiv.querySelector('.circle-bot');
+      });
+      const firstBotMessageDiv = botMessageDivs[0];
+  
+      if (firstBotMessageDiv) {
+        const messageBody = firstBotMessageDiv.querySelector('.message-body');
+        let messageContent = stripHTMLTags(messageBody.innerHTML).trim();
+  
+        if (messageContent === 'Is typing...') {
+            await sleep(50);
+            getFirstMessageBody();
+            return;
+        }
+  
+        if (messageContent !== '') {
+        //   console.info('Sending...', messageContent);
+          window.parent.postMessage(JSON.stringify({"type": "chatResponse", "message": messageContent}), "http://localhost:9000");
+        } else {
+        //   console.log('Ignored message with only whitespace');
+        }
+      } else {
+        // console.log('No message with circle-bot found');
+      }
+    }
+  
+    setInterval(() => {
+      const chatDiv = document.querySelector('.chat');
+      const currentMessageCount = chatDiv.querySelectorAll('.message').length;
+  
+      if (currentMessageCount !== lastMessageCount) {
+        lastMessageCount = currentMessageCount;
+        getFirstMessageBody();
+      }
+    }, 50);
+  }
+  
+  // Call the watchAndGetFirstBotMessage() function to start watching the "chat" div and getting the first bot message
+  watchAndGetFirstBotMessage();
+  
